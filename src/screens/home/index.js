df23a1e6c506d94671b95fbccedbc36f1fec9bf2
theeeee <tkangsilalai@gmail.com>
2022-02-24 16:30:56 +0700

    commit

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,37 +1,131 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
   title: {
     flexGrow: 1,
-    fontSize: "3rem",
+    fontSize: "2.5rem",
+    padding: "2rem",
   },
   text: {
     flexGrow: 1,
     fontSize: "1.5rem",
-    textIndent: "2rem",
+    marginTop: "1rem",
   },
 }));
 
 const HomeScreen = () => {
+  useEffect(() => {
+    fetch("./data.csv")
+      .then((response) => response.text())
+      .then((responseText) => {
+        setQuestions(responseText.split("\r\n").slice(0, 400));
+      })
+      .then(() => {});
+  });
+
   const classes = useStyles();
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [isCorrect, setIsCorrect] = useState();
+  const [isAnswered, setIsAnswered] = useState();
+
+  const handleAnswer = (answer) => {
+    setIsAnswered(true);
+    if (
+      questions[index].split(",")[6].split(" ").join("") ===
+      answer.split(" ").join("")
+    ) {
+      setIsCorrect(true);
+    } else {
+      setIsCorrect(false);
+    }
+  };
 
   return (
-    <div>
-      ASD
-      <Typography variant="h1" className={classes.title}>
-        Home
-      </Typography>
-      <hr />
-      <Typography variant="h1" className={classes.text}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel augue
-        ac felis viverra lacinia. Sed nibh massa, tristique ut fringilla in,
-        molestie id leo. Suspendisse ac dolor non mauris sodales fermentum a ut
-        nunc. Orci varius natoque penatibus et magnis dis parturient montes,
-        nascetur ridiculus mus. Nunc consequat venenatis neque vitae consequat.
-      </Typography>
-    </div>
+    <center>
+      {questions[index] && (
+        <>
+          <Typography variant="h1" className={classes.title}>
+            {questions[index].split(",")[0]}
+          </Typography>
+          <img src={questions[index].split(",")[1]} alt="" />
+
+          <div style={{ width: "80%" }}>
+            <Button
+              className={classes.text}
+              fullWidth
+              variant="outlined"
+              color={isAnswered && !isCorrect ? "secondary" : "primary"}
+              onClick={() =>
+                handleAnswer(questions[index].split(",")[2].replace('"', ""))
+              }
+              disabled={isAnswered}
+            >
+              {questions[index].split(",")[2].replace('"', "")}
+            </Button>
+            <Button
+              className={classes.text}
+              fullWidth
+              variant="outlined"
+              color={isAnswered && !isCorrect ? "secondary" : "primary"}
+              onClick={() =>
+                handleAnswer(questions[index].split(",")[3].replace('"', ""))
+              }
+              disabled={isAnswered}
+            >
+              {questions[index].split(",")[3].replace('"', "")}
+            </Button>
+            <Button
+              className={classes.text}
+              fullWidth
+              variant="outlined"
+              color={isAnswered && !isCorrect ? "secondary" : "primary"}
+              onClick={() =>
+                handleAnswer(questions[index].split(",")[4].replace('"', ""))
+              }
+              disabled={isAnswered}
+            >
+              {questions[index].split(",")[4].replace('"', "")}
+            </Button>
+            <Button
+              className={classes.text}
+              fullWidth
+              variant="outlined"
+              color={isAnswered && !isCorrect ? "secondary" : "primary"}
+              onClick={() =>
+                handleAnswer(questions[index].split(",")[5].replace('"', ""))
+              }
+              disabled={isAnswered}
+            >
+              {questions[index].split(",")[5].replace('"', "")}
+            </Button>
+          </div>
+          {isAnswered &&
+            (isCorrect ? (
+              <Typography variant="h1" className={classes.text}>
+                ถูกต้อง
+              </Typography>
+            ) : (
+              <Typography variant="h1" className={classes.text}>
+                ผิด เฉลย: {questions[index].split(",")[6].replace('"', "")}
+              </Typography>
+            ))}
+          <Button
+            className={classes.text}
+            onClick={() => {
+              setIsCorrect(null);
+              setIsAnswered(false);
+              setIndex(index + 1);
+            }}
+          >
+            ต่อไป {index + 1}/{questions.length}
+          </Button>
+        </>
+      )}
+    </center>
   );
 };
 
